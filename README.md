844f8cf08786289a149db7c08622309442dc2924
mixassio <mixail.bondarev@gmail.com>
2017-05-26 16:00:33 +0300

    first send for check

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
-# Image Resizer
+# Image resize
 
-[TODO. There will be project description]
+The script is designed to resize the image. With the help of the script you will be able to:
+1. Zoom the picture (zoom in or out)
+2. Change the length or width while saving the scaling.
+3. Change the length and width with the loss of scaling
+The file is saved in the same folder (by default) or by specifying the -o flag in the selected folder
+The name of the final file will be pic__200x400.jpg where pic is the name of the source file and 200x400 is the total size.
+
+# How use
+
+Example of script launch on Linux, Python 3.5:
+
+usage: image_resize.py [-height int] [-width int] [-scale int] [-output 'filepath_new'] filepath
+
+```#!bash
+
+$ python image_resize.py ./foto.jpeg -w 400
+
+#
+```
+
 
 # Project Goals
 
--- image_resize.py
+++ image_resize.py
@@ -1,4 +1,5 @@
 import os
+import re
 import argparse
 from PIL import Image
 
@@ -6,19 +7,20 @@ from PIL import Image
 
 def create_parser ():
     parser = argparse.ArgumentParser()
-    parser.add_argument ('filepath')
-    parser.add_argument ('-w', '--width', nargs='?', type=int)
-    parser.add_argument ('-h', '--height', nargs='?', type=int)
-    parser.add_argument ('-s', '--scale', nargs='?', type=float)
-    parser.add_argument ('-o', '--output', nargs='?')
+    parser.add_argument('filepath')
+    parser.add_argument('-w', '--width', nargs='?', type=int)
+    parser.add_argument('-he', '--height', nargs='?', type=int)
+    parser.add_argument('-s', '--scale', nargs='?', type=float)
+    parser.add_argument('-o', '--output', nargs='?')
     return parser
 
 
 def parse_filepath(namespace):
-    filepath_in = namespace.filepath
+    filepath_in = os.path.abspath(namespace.filepath)
+    dir_name, file_name = os.path.split(filepath_in)
     if namespace.output:
         if os.path.exists(namespace.output):
-            filepath_out = namespace.filepath
+            filepath_out = os.path.abspath(namespace.output) + '/' + file_name
         else:
             filepath_out = filepath_in
     else:
@@ -27,23 +29,32 @@ def parse_filepath(namespace):
 
 
 def pic_scale(width, height, scale):
+    print(width, height, scale)
     return int(width*scale), int(height*scale)
 
 
 def check_scale(width, height, new_width, new_height):
-    # TODO
-    print('Scale is broken. The picture is saved with changed proportions')
+    scale_h = new_height / height
+    scale_w = new_width / width
+    if scale_h != scale_w:
+        print('Scale is broken. The picture is saved with changed proportions')
 
-def pic_resize(width, height, new_width=0, new_height=0):
 
-
+def pic_resize(width, height, new_width, new_height):
+    if new_width == 0:
+        scale = new_height / height
+        new_width, new_height = pic_scale(width, height, scale)
+    elif new_height == 0:
+        scale = new_width / width
+        new_width, new_height = pic_scale(width, height, scale)
     return new_width, new_height
 
 
-
-
-
-
+def filepath_save(filepath, width, heigth, format):
+    filepath = os.path.abspath(filepath)
+    dir_name, file_name = os.path.split(filepath)
+    name_file = re.findall('(\w*)\.', os.path.basename(file_name))
+    return dir_name + '/' + name_file[0] + '__' + str(width) + 'x' + str(heigth) +'.' + format
 
 
 if __name__ == '__main__':
@@ -61,13 +72,14 @@ if __name__ == '__main__':
         check_scale(pic.width, pic.height, namespace.width, namespace.height)
         new_width, new_height = namespace.width, namespace.height
     elif namespace.width:
-        new_width, new_height = pic_resize(pic.width, pic.height, namespace.width)
+        new_width, new_height = pic_resize(pic.width, pic.height, namespace.width, 0)
     elif namespace.height:
-        new_width, new_height = pic_resize(pic.width, pic.height, namespace.height)
+        new_width, new_height = pic_resize(pic.width, pic.height, 0, namespace.height)
     else:
         print('You did not enter anything. The program did nothing')
 
     new_im = pic.resize((new_width, new_height))
-    new_im.save(filepath_out, pic.format)
+    save_filepath = filepath_save(filepath_out, new_im.width, new_im.height, pic.format)
+    new_im.save(save_filepath, pic.format)
 
 
